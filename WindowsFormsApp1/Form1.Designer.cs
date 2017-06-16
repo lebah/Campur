7a660486c53f7cc67382081111e1fd2d08390ee3
H.Purwanto <hermawanest@gmail.com>
2017-06-16 11:48:54 +0800

    property tab scope enumeration https://msdn.microsoft.com/en-us/library/0wwd4w1c(v=vs.110).aspx

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -35,6 +35,7 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -45,12 +46,11 @@
             this.tabControl1.Location = new System.Drawing.Point(0, 0);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(284, 262);
+            this.tabControl1.Size = new System.Drawing.Size(1419, 607);
             this.tabControl1.TabIndex = 1;
             // 
             // tabPage1
             // 
-            this.tabPage1.Controls.Add(this.textBox2);
             this.tabPage1.Controls.Add(this.textBox1);
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
@@ -73,26 +73,27 @@
             // 
             // tabPage2
             // 
+            this.tabPage2.Controls.Add(this.textBox2);
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(276, 236);
+            this.tabPage2.Size = new System.Drawing.Size(1411, 581);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "tabPage2";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(8, 8);
+            this.textBox2.Location = new System.Drawing.Point(45, 18);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 2;
+            this.textBox2.TabIndex = 3;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(1419, 607);
             this.Controls.Add(this.tabControl1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -100,6 +101,8 @@
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
             this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
             this.ResumeLayout(false);
 
         }
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -1,14 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Globalization;
-using System.Linq;
+using System.IO;
 using System.Reflection;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.Design;
 
 namespace WindowsFormsApp1
 {
@@ -16,7 +17,26 @@ namespace WindowsFormsApp1
     {
         public Form1()
         {
-            InitializeComponent();
+            InitializeComponent();            ;
+            InitPropertyGrid();
+        }
+
+        private void InitPropertyGrid()
+        {
+            // The initial constructor code goes here.
+
+            PropertyGrid propertyGrid1 = new PropertyGrid();
+            propertyGrid1.CommandsVisibleIfAvailable = true;
+            propertyGrid1.Location = new Point(10, 20);
+            propertyGrid1.Size = new System.Drawing.Size(400, 300);
+            propertyGrid1.TabIndex = 1;
+            propertyGrid1.Text = "Property Grid";
+            propertyGrid1.Dock = DockStyle.Right;
+            propertyGrid1.PropertyTabs.AddTabType(typeof(TypeCategoryTab));
+
+            this.tabPage2.Controls.Add(propertyGrid1);
+
+            propertyGrid1.SelectedObject = textBox2;
         }
 
         private void DisplayTypeInfo()
@@ -46,6 +66,86 @@ namespace WindowsFormsApp1
         private void Form1_Load(object sender, EventArgs e)
         {
             DisplayTypeInfo();
+        }
+    }
+
+    // This component adds a TypeCategoryTab to the property browser 
+    // that is available for any components in the current design mode document.
+    [PropertyTabAttribute(typeof(TypeCategoryTab), PropertyTabScope.Document)]
+    public class TypeCategoryTabComponent : System.ComponentModel.Component
+    {
+        public TypeCategoryTabComponent()
+        {
+        }
+    }
+
+    // A TypeCategoryTab property tab lists properties by the 
+    // category of the type of each property.
+    [System.Security.Permissions.PermissionSet(System.Security.Permissions.SecurityAction.Demand, Name = "FullTrust")]
+    public class TypeCategoryTab : PropertyTab
+    {
+        [Browsable(true)]
+        // This string contains a Base-64 encoded and serialized example property tab image.
+        private string img = "AAEAAAD/////AQAAAAAAAAAMAgAAAFRTeXN0ZW0uRHJhd2luZywgVmVyc2lvbj0xLjAuMzMwMC4wLCBDdWx0dXJlPW5ldXRyYWwsIFB1YmxpY0tleVRva2VuPWIwM2Y1ZjdmMTFkNTBhM2EFAQAAABVTeXN0ZW0uRHJhd2luZy5CaXRtYXABAAAABERhdGEHAgIAAAAJAwAAAA8DAAAA9gAAAAJCTfYAAAAAAAAANgAAACgAAAAIAAAACAAAAAEAGAAAAAAAAAAAAMQOAADEDgAAAAAAAAAAAAD///////////////////////////////////9ZgABZgADzPz/zPz/zPz9AgP//////////gAD/gAD/AAD/AAD/AACKyub///////+AAACAAAAAAP8AAP8AAP9AgP////////9ZgABZgABz13hz13hz13hAgP//////////gAD/gACA/wCA/wCA/wAA//////////+AAACAAAAAAP8AAP8AAP9AgP////////////////////////////////////8L";
+
+        public TypeCategoryTab()
+        {
+        }
+
+        // Returns the properties of the specified component extended with 
+        // a CategoryAttribute reflecting the name of the type of the property.
+        public override PropertyDescriptorCollection GetProperties(object component, Attribute[] attributes)
+        {
+            PropertyDescriptorCollection props;
+            if (attributes == null)
+                props = TypeDescriptor.GetProperties(component);
+            else
+                props = TypeDescriptor.GetProperties(component, attributes);
+
+            PropertyDescriptor[] propArray = new PropertyDescriptor[props.Count];
+            for (int i = 0; i < props.Count; i++)
+            {
+                // Create a new PropertyDescriptor from the old one, with 
+                // a CategoryAttribute matching the name of the type.
+                propArray[i] = TypeDescriptor.CreateProperty(props[i].ComponentType, props[i], new CategoryAttribute(props[i].PropertyType.Name));
+            }
+            return new PropertyDescriptorCollection(propArray);
+        }
+
+        public override PropertyDescriptorCollection GetProperties(object component)
+        {
+            return this.GetProperties(component, null);
+        }
+
+        // Provides the name for the property tab.
+        public override string TabName
+        {
+            get
+            {
+                return "Properties by Type 2";
+            }
+        }
+
+        // Provides an image for the property tab.
+        public override Bitmap Bitmap
+        {
+            get
+            {
+                Bitmap bmp = new Bitmap(DeserializeFromBase64Text(img));
+                return bmp;
+            }
+        }
+
+        // This method can be used to retrieve an Image from a block of Base64-encoded text.
+        private Image DeserializeFromBase64Text(string text)
+        {
+            Image img = null;
+            byte[] memBytes = Convert.FromBase64String(text);
+            IFormatter formatter = new BinaryFormatter();
+            MemoryStream stream = new MemoryStream(memBytes);
+            img = (Image)formatter.Deserialize(stream);
+            stream.Close();
+            return img;
         }
     }
 }
